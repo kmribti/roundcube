973dfdb79cdf8a52ee518546305769a2892eac69
till <till@208e9e7b-5314-0410-a742-e7e81cd9613c>
2007-06-05 16:26:02 +0000

    + removed patch - fixed a bug i introduced yesterdayish (send flag) + don't allow GET to bin/html2text.php
    git-svn-id: https://svn.roundcube.net/branches/devel-vnext@595 208e9e7b-5314-0410-a742-e7e81cd9613c

--- bin/html2text.php
+++ bin/html2text.php
@@ -1,16 +1,20 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    echo 'Method not allowed.';
+    exit;
+}
+require_once dirname(__FILE__) . '/../program/lib/html2text.inc';
 
-require_once('../program/lib/html2text.inc');
-
-$htmlText = $HTTP_RAW_POST_DATA;
+$htmlText  = $HTTP_RAW_POST_DATA;
 $converter = new html2text($htmlText);
 
 header('Content-Type: text/plain; charset=UTF-8');
 $plaintext = $converter->get_text();
 
-if (function_exists('html_entity_decode'))
-	print html_entity_decode($plaintext, ENT_COMPAT, 'UTF-8');
-else
-	print $plaintext;
-
+if (function_exists('html_entity_decode')) {
+    echo html_entity_decode($plaintext, ENT_COMPAT, 'UTF-8');
+}
+else {
+    echo $plaintext;
+}
 ?>
