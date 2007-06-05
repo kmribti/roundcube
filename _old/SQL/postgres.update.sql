59d914a49db6b378c06b3953604df8697972ea8b
till <till@208e9e7b-5314-0410-a742-e7e81cd9613c>
2007-06-05 16:46:49 +0000

    + CS and debugging in rcube_imap - removing _old
    git-svn-id: https://svn.roundcube.net/branches/devel-vnext@597 208e9e7b-5314-0410-a742-e7e81cd9613c

--- _old/SQL/postgres.update.sql
+++
@@ -1,9 +0,0 @@
--- RoundCube Webmail update script for Postres databases
--- Updates from version 0.1-beta and older
-
-ALTER TABLE "messages" DROP body;
-ALTER TABLE "messages" ADD structure TEXT;
-ALTER TABLE "messages" ADD UNIQUE (cache_key, uid);
-
-ALTER TABLE "identities" ADD html_signature integer DEFAULT 0 NOT NULL;
-
