a8a26f2d9e5232e7f30fab25a5db7a6c8b6ad2f9
till <till@208e9e7b-5314-0410-a742-e7e81cd9613c>
2007-06-18 11:40:27 +0000

    + registry     * implements a singleton (hopefully)     * php4 and 5 compatible
    git-svn-id: https://svn.roundcube.net/branches/devel-vnext@621 208e9e7b-5314-0410-a742-e7e81cd9613c

--- program/include/rcube/registry.php
+++ program/include/rcube/registry.php
@@ -2,74 +2,160 @@
 /**
  * rcRegistry
  *
+ * Implements a singleton.
+ *
  * @final
  * @author Till Klampaeckel <till@php.net>
  * @since  0.1-rc1
  */
-class rcRegistry
+class rc_registry
 {
-    function rcRegistry()
+    var $holder = null;
+
+    /**
+     * rc_registry
+     *
+     * @return false
+     */
+    function rc_registry()
     {
         return false;
     }
 
+    /**
+     * __construct
+     *
+     * @uses   rc_registry::rc_registry()
+     * @return false
+     */
     function __construct()
     {
-        return false;
+        return $this->rc_registry();
     }
 
+    /**
+     * getInstance
+     *
+     * Returns rc_registry.
+     *
+     * @access static
+     * @return rc_registry
+     */
+    function getInstance()
+    {
+        static $registry = null;
+        if (is_null($registry) === true)
+        {
+            $registry = new rc_registry;
+        }
+        return $registry;
+    }
+
+    /**
+     * set
+     *
+     * Saves a variable (by name and value) into the registry. Also takes an
+     * optional argument $ns (namespace).
+     *
+     * Returns boolean - true on success, false if things go wrong.
+     *
+     * @access public
+     * @param  string $var
+     * @param  mixed $val
+     * @param  string $ns
+     * @return boolean
+     * @uses   rc_registry::$holder
+     */
     function set($var, $val = '', $ns = null)
     {
         if (empty($var) === true) {
             return false;
         }
-        if (isset($GLOBALS['rcRegistry']) === false) {
-            $GLOBALS['rcRegistry'] = array();
-        }
         if (is_null($ns) === true) {
-            $GLOBALS['rcRegistry'][$var] = $val;
+            $this->holder[$var] = $val;
             return true;
         }
-        if (isset($GLOBALS['rcRegistry'][$ns]) === false) {
-            $GLOBALS['rcRegistry'][$ns] = array();
+        if (isset($this->holder[$ns]) === false) {
+            $this->holder[$ns] = array();
         }
-        $GLOBALS['rcRegistry'][$ns][$var] = $val;
+        $this->holder[$ns][$var] = $val;
         return true;
     }
 
+    /**
+     * get
+     *
+     * Returns a variable - by name. Also uses $ns (namespace) if supplied.
+     * Returns boolean (false) when things go wrong - and the variable otherwise.
+     *
+     * @access public
+     * @todo   The return value 'blows' - false could be in the variable. ;-)
+     * @param  string $var
+     * @param  string $ns
+     * @return mixed
+     * @uses   rc_registry::$holder
+     */
     function get($var, $ns = null)
     {
         if (empty($var) === true) {
             return false;
         }
         if (is_null($ns) === true) {
-            if (isset($GLOBALS['rcRegistry'][$var]) === false) {
+            if (isset($this->holder[$var]) === false) {
                 return false;
             }
-            return $GLOBALS['rcRegistry'][$var];
+            return $this->holder[$var];
         }
-        if (isset($GLOBALS['rcRegistry'][$ns]) === false) {
+        if (isset($this->holder[$ns]) === false) {
             return false;
         }
-        if (isset($GLOBALS['rcRegistry'][$ns][$var]) === false) {
+        if (isset($this->holder[$ns][$var]) === false) {
             return false;
         }
-        return $GLOBALS['rcRegistry'][$ns][$var];
+        return $this->holder[$ns][$var];
     }
 
+    /**
+     * purge
+     *
+     * Cleans the $holder
+     *
+     * @access public
+     * @return boolean
+     * @uses   rc_registry::$holder
+     * @see    rc_registry::__destruct
+     */
     function purge()
     {
-        unset($GLOBALS['rcRegistry']);
+        $this->holder = null;
         return true;
     }
 
+    /**
+     * getAll
+     *
+     * Returns the entire registry, or false if it's not set.
+     *
+     * @access public
+     * @return mixed
+     */
     function getAll()
     {
-        if (isset($GLOBALS['rcRegistry']) === false) {
+        if (is_null($this->holder) === true) {
             return false;
         }
-        return $GLOBALS['rcRegistry'];
+        return $this->holder;
+    }
+
+    /**
+     * __destruct
+     *
+     * @return boolean
+     * @uses   rc_registry::purge()
+     */
+    function __destruct()
+    {
+        return $this->purge();
     }
 }
-
 ?>
