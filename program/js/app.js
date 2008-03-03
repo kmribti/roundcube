kmribti/roundcube

commit 206944f797628ef41031c211b703da4d2fcdb04e

Minor bug fixes and visual enhancements
git-svn-id: https://svn.roundcube.net/trunk/roundcubemail@1167 208e9e7b-5314-0410-a742-e7e81cd9613c

program/js/app.js
@@ -1387,11 +1387,8 @@ function rcube_webmail()
   this.move_messages = function(mbox)
     {
     // exit if current or no mailbox specified or if selection is empty
-    if (!mbox || !this.env.uid || mbox==this.env.mailbox)
-      {
-      if (!this.message_list || !this.message_list.get_selection().length)
-        return;
-      }
+    if (!mbox || !this.env.uid || mbox == this.env.mailbox || !this.message_list || !this.message_list.get_selection().length)
+      return;
 
     var lock = false;
     var add_url = '&_target_mbox='+urlencode(mbox)+'&_from='+(this.env.action ? this.env.action : '');

program/include/rcube_html.inc
@@ -636,7 +636,7 @@ class select extends rcube_form_element
     $value_str = $this->_conv_case(' value="%s"', 'attrib');
     
     if (!is_array($select))
-      $select = array((string)$select);
+      $select = array($select);
 
     foreach ($this->options as $option)
     {

skins/default/mail.css
@@ -323,6 +323,11 @@ td.formlinks a:visited
 #mailboxlist li.droptarget li.selected
 {
   background-color: #929292;
+}
+
+#mailboxlist li.selected a,
+#mailboxlist li.droptarget li.selected a
+{
   color: #FFF;
   font-weight: bold;
 }

commit 3b6520b4a9dc19853561915c1b16318e76d667c9

Fix bug introduced with r1167
git-svn-id: https://svn.roundcube.net/trunk/roundcubemail@1170 208e9e7b-5314-0410-a742-e7e81cd9613c

program/js/app.js
@@ -1095,15 +1095,11 @@ function rcube_webmail()
   // onmouseup handler for folder list item
   this.folder_mouse_up = function(id)
     {
-    // Hide message command buttons until a message is selected
-    this.enable_command('reply', 'reply-all', 'forward', 'delete', 'mark', 'print', false);
-
     if (this.drag_active)
       {
       this.unfocus_folder(id);
       this.command('moveto', id);
       }
-
     };
 
   this.click_on_list = function(e)
@@ -1387,7 +1383,7 @@ function rcube_webmail()
   this.move_messages = function(mbox)
     {
     // exit if current or no mailbox specified or if selection is empty
-    if (!mbox || !this.env.uid || mbox == this.env.mailbox || !this.message_list || !this.message_list.get_selection().length)
+    if (!mbox || mbox == this.env.mailbox || (!this.env.uid && (!this.message_list || !this.message_list.get_selection().length)))
       return;
 
     var lock = false;
@@ -1401,6 +1397,9 @@ function rcube_webmail()
       }
     else
       this.show_contentframe(false);
+
+    // Hide message command buttons until a message is selected
+    this.enable_command('reply', 'reply-all', 'forward', 'delete', 'mark', 'print', false);
 
     this._with_selected_messages('moveto', lock, add_url);
     };