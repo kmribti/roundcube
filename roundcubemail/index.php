kmribti/roundcube

commit 456b357481dea5776143ea17ec9c13a2ab9e90ec

- Fix setting task name according to auth state. So, any action before user   is authenticated is assigned to 'login' task instead of 'mail'. Now binding   plugins to 'login' task is possible and realy usefull. It's also possible   to bind to all tasks excluding 'login'.
git-svn-id: https://svn.roundcube.net/trunk@3258 208e9e7b-5314-0410-a742-e7e81cd9613c

roundcubemail/index.php
@@ -80,7 +80,7 @@ $RCMAIL->set_task($startup['task']);
 $RCMAIL->action = $startup['action'];
 
 // try to log in
-if ($RCMAIL->action=='login' && $RCMAIL->task=='mail') {
+if ($RCMAIL->task == 'login' && $RCMAIL->action == 'login') {
   // purge the session in case of new login when a session already exists 
   $RCMAIL->kill_session();
   
@@ -117,6 +117,8 @@ if ($RCMAIL->action=='login' && $RCMAIL->task=='mail') {
     if ($url = get_input_value('_url', RCUBE_INPUT_POST))
       parse_str($url, $query);
 
+    $RCMAIL->set_task('mail');
+
     // allow plugins to control the redirect url after login success
     $redir = $RCMAIL->plugins->exec_hook('login_after', $query + array('task' => $RCMAIL->task));
     unset($redir['abort']);
@@ -132,7 +134,7 @@ if ($RCMAIL->action=='login' && $RCMAIL->task=='mail') {
 }
 
 // end session
-else if ($RCMAIL->task=='logout' && isset($_SESSION['user_id'])) {
+else if ($RCMAIL->task == 'logout' && isset($_SESSION['user_id'])) {
   $userdata = array('user' => $_SESSION['username'], 'host' => $_SESSION['imap_host'], 'lang' => $RCMAIL->user->language);
   $OUTPUT->show_message('loggedout');
   $RCMAIL->logout_actions();
@@ -141,7 +143,7 @@ else if ($RCMAIL->task=='logout' && isset($_SESSION['user_id'])) {
 }
 
 // check session and auth cookie
-else if ($RCMAIL->action != 'login' && $_SESSION['user_id'] && $RCMAIL->action != 'send') {
+else if ($RCMAIL->task != 'login' && $_SESSION['user_id'] && $RCMAIL->action != 'send') {
   if (!$RCMAIL->authenticate_session()) {
     $OUTPUT->show_message('sessionerror', 'error');
     $RCMAIL->kill_session();
@@ -168,7 +170,7 @@ else if (!empty($_POST) && !$request_check_whitelist[$RCMAIL->action] && !$RCMAI
 if (empty($RCMAIL->user->ID)) {
   if ($OUTPUT->ajax_call)
     $OUTPUT->redirect(array(), 2000);
-  
+
   if (!empty($_REQUEST['_framed']))
     $OUTPUT->command('redirect', '?');
 

roundcubemail/program/include/rcmail.php
@@ -39,7 +39,7 @@ class rcmail
   public $imap;
   public $output;
   public $plugins;
-  public $task = 'mail';
+  public $task;
   public $action = '';
   public $comm_path = './';
   
@@ -91,10 +91,6 @@ class rcmail
       openlog($syslog_id, LOG_ODELAY, $syslog_facility);
     }
 
-    // set task and action properties
-    $this->set_task(get_input_value('_task', RCUBE_INPUT_GPC));
-    $this->action = asciiwords(get_input_value('_action', RCUBE_INPUT_GPC));
-
     // connect to database
     $GLOBALS['DB'] = $this->get_dbh();
 
@@ -123,6 +119,10 @@ class rcmail
     // create user object
     $this->set_user(new rcube_user($_SESSION['user_id']));
 
+    // set task and action properties
+    $this->set_task(get_input_value('_task', RCUBE_INPUT_GPC));
+    $this->action = asciiwords(get_input_value('_action', RCUBE_INPUT_GPC));
+
     // reset some session parameters when changing task
     if ($_SESSION['task'] != $this->task)
       rcube_sess_unset('page');
@@ -131,7 +131,7 @@ class rcmail
     $_SESSION['task'] = $this->task;
 
     // create IMAP object
-    if ($this->task == 'mail')
+    if ($this->task == 'login')
       $this->imap_init();
       
     // create plugin API and load plugins
@@ -147,7 +147,13 @@ class rcmail
   public function set_task($task)
   {
     $task = asciiwords($task);
-    $this->task = $task ? $task : 'mail';
+
+    if ($this->user && $this->user->ID)
+      $task = !$task || $task == 'login' ? 'mail' : $task;
+    else
+      $task = 'login';
+
+    $this->task = $task;
     $this->comm_path = $this->url(array('task' => $this->task));
     
     if ($this->output)

roundcubemail/program/include/rcube_plugin_api.php
@@ -90,7 +90,7 @@ class rcube_plugin_api
         if (class_exists($plugin_name, false)) {
           $plugin = new $plugin_name($this);
           // check inheritance and task specification
-          if (is_subclass_of($plugin, 'rcube_plugin') && (!$plugin->task || preg_match('/('.$plugin->task.')/i', $rcmail->task))) {
+          if (is_subclass_of($plugin, 'rcube_plugin') && (!$plugin->task || preg_match('/^('.$plugin->task.')$/i', $rcmail->task))) {
             $plugin->init();
             $this->plugins[] = $plugin;
           }

roundcubemail/program/steps/mail/func.inc
@@ -24,6 +24,8 @@ $EMAIL_ADDRESS_PATTERN = '([a-z0-9][a-z0-9\-\.\+\_]*@[a-z0-9][a-z0-9\-\.]*\\.[a-
 // actions that do not require imap connection
 $NOIMAP_ACTIONS = array('spell', 'addcontact', 'autocomplete', 'upload', 'display-attachment', 'remove-attachment');
 
+// Init IMAP object
+$RCMAIL->imap_init();
 
 // log in to imap server
 if (!in_array($RCMAIL->action, $NOIMAP_ACTIONS) && !$RCMAIL->imap_connect()) {

roundcubemail/plugins/archive/archive.php
@@ -17,9 +17,6 @@ class archive extends rcube_plugin
   {
     $rcmail = rcmail::get_instance();
 
-    if (!$rcmail->user->ID)
-      return;
-
     $this->register_action('plugin.archive', array($this, 'request_action'));
 
     // There is no "Archived flags"

roundcubemail/plugins/autologon/autologon.php
@@ -6,6 +6,7 @@
  */
 class autologon extends rcube_plugin
 {
+  public $task = 'login';
 
   function init()
   {
@@ -18,7 +19,7 @@ class autologon extends rcube_plugin
     $rcmail = rcmail::get_instance();
 
     // change action to login
-    if ($args['task'] == 'mail' && empty($args['action']) && empty($_SESSION['user_id']) && !empty($_GET['_autologin']) && $this->is_localhost())
+    if (empty($_SESSION['user_id']) && !empty($_GET['_autologin']) && $this->is_localhost())
       $args['action'] = 'login';
 
     return $args;

roundcubemail/plugins/help/help.php
@@ -12,13 +12,13 @@
 
 class help extends rcube_plugin
 {
+    // all task excluding 'login' and 'logout'
+    public $task = '?(?!login|logout).*';
+
     function init()
     {
       $rcmail = rcmail::get_instance();
       
-      if (!$rcmail->user->ID)
-        return;
-
       $this->add_texts('localization/', false);
       
       // register actions

roundcubemail/plugins/http_authentication/http_authentication.php
@@ -10,6 +10,7 @@
  */
 class http_authentication extends rcube_plugin
 {
+  public $task = 'login';
 
   function init()
   {
@@ -20,7 +21,7 @@ class http_authentication extends rcube_plugin
   function startup($args)
   {
     // change action to login
-    if ($args['task'] == 'mail' && empty($args['action']) && empty($_SESSION['user_id'])
+    if (empty($args['action']) && empty($_SESSION['user_id'])
         && !empty($_SERVER['PHP_AUTH_USER']) && !empty($_SERVER['PHP_AUTH_PW']))
       $args['action'] = 'login';
 

commit 06df7153a7794f6b52f58f81f5e955451d99976e

- Fix imap_init hook broken in r3258 (#1486493)
git-svn-id: https://svn.roundcube.net/trunk@3268 208e9e7b-5314-0410-a742-e7e81cd9613c

roundcubemail/index.php
@@ -30,14 +30,8 @@
 // include environment
 require_once 'program/include/iniset.php';
 
-// init application and start session with requested task
+// init application, start session, init output class, etc.
 $RCMAIL = rcmail::get_instance();
-
-// init output class
-$OUTPUT = !empty($_REQUEST['_remote']) ? $RCMAIL->init_json() : $RCMAIL->load_gui(!empty($_REQUEST['_framed']));
-
-// init plugin API
-$RCMAIL->plugins->init();
 
 // turn on output buffering
 ob_start();

roundcubemail/program/include/rcmail.php
@@ -130,12 +130,17 @@ class rcmail
     // set current task to session
     $_SESSION['task'] = $this->task;
 
-    // create IMAP object
-    if ($this->task == 'login')
-      $this->imap_init();
-      
+    // init output class
+    if (!empty($_REQUEST['_remote']))
+      $GLOBALS['OUTPUT'] = $this->init_json();
+    else
+      $GLOBALS['OUTPUT'] = $this->load_gui(!empty($_REQUEST['_framed']));
+
     // create plugin API and load plugins
     $this->plugins = rcube_plugin_api::get_instance();
+
+    // init plugins
+    $this->plugins->init();
   }
   
   
@@ -395,7 +400,11 @@ class rcmail
   
     // set global object for backward compatibility
     $GLOBALS['IMAP'] = $this->imap;
-    
+
+    $hook = $this->plugins->exec_hook('imap_init', array('fetch_headers' => $this->imap->fetch_add_headers));
+    if ($hook['fetch_headers'])
+      $this->imap->fetch_add_headers = $hook['fetch_headers'];
+				        
     if ($connect)
       $this->imap_connect();
   }
@@ -409,6 +418,9 @@ class rcmail
   public function imap_connect()
   {
     $conn = false;
+
+    if (!$this->imap)
+      $this->imap_init();
     
     if ($_SESSION['imap_host'] && !$this->imap->conn) {
       if (!($conn = $this->imap->connect($_SESSION['imap_host'], $_SESSION['username'], $this->decrypt($_SESSION['password']), $_SESSION['imap_port'], $_SESSION['imap_ssl']))) {
@@ -493,6 +505,9 @@ class rcmail
     // user already registered -> overwrite username
     if ($user = rcube_user::query($username, $host))
       $username = $user->data['username'];
+
+    if (!$this->imap)
+      $this->imap_init();
 
     // exit if IMAP login failed
     if (!($imap_login  = $this->imap->connect($host, $username, $pass, $imap_port, $imap_ssl)))

roundcubemail/program/include/rcube_plugin_api.php
@@ -150,15 +150,6 @@ class rcube_plugin_api
     $this->register_hook('template_container', array($this, 'template_container_hook'));
     
     // maybe also register a shudown function which triggers shutdown functions of all plugin objects
-    
-    
-    // call imap_init right now
-    // (should actually be done in rcmail::imap_init() but plugins are not initialized then)
-    if ($rcmail->imap) {
-      $hook = $this->exec_hook('imap_init', array('fetch_headers' => $rcmail->imap->fetch_add_headers));
-      if ($hook['fetch_headers'])
-        $rcmail->imap->fetch_add_headers = $hook['fetch_headers'];
-    }
   }
   
   

roundcubemail/program/steps/mail/func.inc
@@ -24,9 +24,6 @@ $EMAIL_ADDRESS_PATTERN = '([a-z0-9][a-z0-9\-\.\+\_]*@[a-z0-9][a-z0-9\-\.]*\\.[a-
 // actions that do not require imap connection
 $NOIMAP_ACTIONS = array('spell', 'addcontact', 'autocomplete', 'upload', 'display-attachment', 'remove-attachment');
 
-// Init IMAP object
-$RCMAIL->imap_init();
-
 // log in to imap server
 if (!in_array($RCMAIL->action, $NOIMAP_ACTIONS) && !$RCMAIL->imap_connect()) {
   $RCMAIL->kill_session();
@@ -42,7 +39,7 @@ if (!in_array($RCMAIL->action, $NOIMAP_ACTIONS) && !$RCMAIL->imap_connect()) {
 // set imap properties and session vars
 if ($mbox = get_input_value('_mbox', RCUBE_INPUT_GPC))
   $IMAP->set_mailbox(($_SESSION['mbox'] = $mbox));
-else
+else if ($IMAP)
   $_SESSION['mbox'] = $IMAP->get_mailbox_name();
 
 if (!empty($_GET['_page']))