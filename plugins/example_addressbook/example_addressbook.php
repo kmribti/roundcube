9bc4e105b040fcde268cd04a4cca3803f935119a
alec <alec@208e9e7b-5314-0410-a742-e7e81cd9613c>
2010-05-04 11:16:57 +0000

    - Fix PHP Warning when autocomplete_addressbooks is not an array
    git-svn-id: https://svn.roundcube.net/trunk@3591 208e9e7b-5314-0410-a742-e7e81cd9613c

--- plugins/example_addressbook/example_addressbook.php
+++ plugins/example_addressbook/example_addressbook.php
@@ -16,7 +16,7 @@ class example_addressbook extends rcube_plugin
     // use this address book for autocompletion queries
     // (maybe this should be configurable by the user?)
     $config = rcmail::get_instance()->config;
-    $sources = $config->get('autocomplete_addressbooks', array('sql'));
+    $sources = (array) $config->get('autocomplete_addressbooks', array('sql'));
     if (!in_array($this->abook_id, $sources)) {
       $sources[] = $this->abook_id;
       $config->set('autocomplete_addressbooks', $sources);
