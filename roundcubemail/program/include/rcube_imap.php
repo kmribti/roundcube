kmribti/roundcube

commit 9b06107f55a726f152dd093996b34f926eab123b

- Workaround for setting charset in case of malformed bodystructure response (#1487700)
git-svn-id: https://svn.roundcube.net/trunk@4421 208e9e7b-5314-0410-a742-e7e81cd9613c

roundcubemail/program/include/rcube_imap.php
@@ -2053,7 +2053,7 @@ class rcube_imap
                 return false;
         }
 
-        $struct = &$this->_structure_part($structure);
+        $struct = &$this->_structure_part($structure, 0, '', $headers);
         $struct->headers = get_object_vars($headers);
 
         // don't trust given content-type
@@ -2183,6 +2183,11 @@ class rcube_imap
 
             if (isset($struct->ctype_parameters['charset']))
                 $struct->charset = $struct->ctype_parameters['charset'];
+        }
+
+        // #1487700: workaround for lack of charset in malformed structure
+        if (empty($struct->charset) && !empty($mime_headers) && $mime_headers->charset) {
+            $struct->charset = $mime_headers->charset;
         }
 
         // read content encoding

commit 9ea1b334066e0da305004959c892e308f05ef276

Check mime headers before attempt to parse them (#1487745); broken since r4421
git-svn-id: https://svn.roundcube.net/trunk@4468 208e9e7b-5314-0410-a742-e7e81cd9613c

roundcubemail/program/include/rcube_imap.php
@@ -2238,7 +2238,11 @@ class rcube_imap
                 $mime_headers = $this->conn->fetchPartHeader(
                     $this->mailbox, $this->_msg_id, false, $struct->mime_id);
             }
-            $struct->headers = $this->_parse_headers($mime_headers) + $struct->headers;
+
+            if (is_string($mime_headers))
+                $struct->headers = $this->_parse_headers($mime_headers) + $struct->headers;
+            else if (is_object($mime_headers))
+                $struct->headers = get_object_vars($mime_headers) + $struct->headers;
 
             // get real content-type of message/rfc822
             if ($struct->mimetype == 'message/rfc822') {