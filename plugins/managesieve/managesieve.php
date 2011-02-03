74c5145a37f7da99158c40728d7d110641047a98
alec <alec@208e9e7b-5314-0410-a742-e7e81cd9613c>
2011-02-03 13:58:19 +0000

    - Fix IDNA support when IDN/INTL modules are in use (#1487742)
    git-svn-id: https://svn.roundcube.net/trunk@4485 208e9e7b-5314-0410-a742-e7e81cd9613c

--- plugins/managesieve/managesieve.php
+++ plugins/managesieve/managesieve.php
@@ -66,7 +66,7 @@ class managesieve extends rcube_plugin
         $host = rcube_parse_host($this->rc->config->get('managesieve_host', 'localhost'));
         $port = $this->rc->config->get('managesieve_port', 2000);
 
-        $host = idn_to_ascii($host);
+        $host = rcube_idn_to_ascii($host);
 
         // try to connect to managesieve server and to fetch the script
         $this->sieve = new rcube_sieve($_SESSION['username'],
