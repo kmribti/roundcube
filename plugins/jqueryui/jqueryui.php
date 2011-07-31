25b0dcea65f59b72175daaa414142a6be7dd51fc
thomasb <thomasb@208e9e7b-5314-0410-a742-e7e81cd9613c>
2011-07-31 08:51:09 +0000

    Update version strings in meta info
    git-svn-id: https://svn.roundcube.net/trunk@4993 208e9e7b-5314-0410-a742-e7e81cd9613c

--- plugins/jqueryui/jqueryui.php
+++ plugins/jqueryui/jqueryui.php
@@ -5,7 +5,7 @@
  * 
  * Provide the jQuery UI library with according themes.
  * 
- * @version 1.8.12
+ * @version 1.8.14
  * @author Cor Bosman <roundcube@wa.ter.net>
  * @author Thomas Bruederli <roundcube@gmail.com>
  */
