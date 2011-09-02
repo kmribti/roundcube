kmribti/roundcube

commit 0e75c95acf063e482281e028c6e21b478864017f

Initialize GUI objects which are added after init
git-svn-id: https://svn.roundcube.net/trunk@5146 208e9e7b-5314-0410-a742-e7e81cd9613c

roundcubemail/program/js/app.js
@@ -86,12 +86,15 @@ function rcube_webmail()
     if (over) button_prop.over = over;
 
     this.buttons[command].push(button_prop);
+    
+    if (this.loaded)
+      init_button(command, button_prop);
   };
 
   // register a specific gui object
   this.gui_object = function(name, id)
   {
-    this.gui_objects[name] = id;
+    this.gui_objects[name] = this.loaded ? rcube_find_object(id) : id;
   };
 
   // register a container object
@@ -4916,6 +4919,34 @@ function rcube_webmail()
   /*********           GUI functionality           *********/
   /*********************************************************/
 
+  var init_button = function(cmd, prop)
+  {
+    var elm = document.getElementById(prop.id);
+    if (!elm)
+      return;
+
+    var preload = false;
+    if (prop.type == 'image') {
+      elm = elm.parentNode;
+      preload = true;
+    }
+
+    elm._command = cmd;
+    elm._id = prop.id;
+    if (prop.sel) {
+      elm.onmousedown = function(e){ return rcmail.button_sel(this._command, this._id); };
+      elm.onmouseup = function(e){ return rcmail.button_out(this._command, this._id); };
+      if (preload)
+        new Image().src = prop.sel;
+    }
+    if (prop.over) {
+      elm.onmouseover = function(e){ return rcmail.button_over(this._command, this._id); };
+      elm.onmouseout = function(e){ return rcmail.button_out(this._command, this._id); };
+      if (preload)
+        new Image().src = prop.over;
+    }
+  };
+
   // enable/disable buttons for page shifting
   this.set_page_buttons = function()
   {
@@ -4931,31 +4962,7 @@ function rcube_webmail()
         continue;
 
       for (var i=0; i< this.buttons[cmd].length; i++) {
-        var prop = this.buttons[cmd][i];
-        var elm = document.getElementById(prop.id);
-        if (!elm)
-          continue;
-
-        var preload = false;
-        if (prop.type == 'image') {
-          elm = elm.parentNode;
-          preload = true;
-        }
-
-        elm._command = cmd;
-        elm._id = prop.id;
-        if (prop.sel) {
-          elm.onmousedown = function(e){ return rcmail.button_sel(this._command, this._id); };
-          elm.onmouseup = function(e){ return rcmail.button_out(this._command, this._id); };
-          if (preload)
-            new Image().src = prop.sel;
-        }
-        if (prop.over) {
-          elm.onmouseover = function(e){ return rcmail.button_over(this._command, this._id); };
-          elm.onmouseout = function(e){ return rcmail.button_out(this._command, this._id); };
-          if (preload)
-            new Image().src = prop.over;
-        }
+        init_button(cmd, this.buttons[cmd][i]);
       }
     }
   };
@@ -5568,7 +5575,7 @@ function rcube_webmail()
     var base = this.env.comm_path;
 
     // overwrite task name
-    if (query._action.match(/([a-z]+)\/([a-z-_]+)/)) {
+    if (query._action.match(/([a-z]+)\/([a-z-_.]+)/)) {
       query._action = RegExp.$2;
       base = base.replace(/\_task=[a-z]+/, '_task='+RegExp.$1);
     }

commit 241459e7bdf173339b111133af2d5c0664d9cea4

- Fix error in messages drag&drop action caused by r5146
git-svn-id: https://svn.roundcube.net/trunk@5153 208e9e7b-5314-0410-a742-e7e81cd9613c

roundcubemail/program/js/app.js
@@ -1213,12 +1213,12 @@ function rcube_webmail()
   this.drag_menu = function(e, target)
   {
     var modkey = rcube_event.get_modifier(e),
-      menu = $('#'+this.gui_objects.message_dragmenu);
+      menu = this.gui_objects.message_dragmenu;
 
     if (menu && modkey == SHIFT_KEY && this.commands['copy']) {
       var pos = rcube_event.get_mouse_pos(e);
       this.env.drag_target = target;
-      menu.css({top: (pos.y-10)+'px', left: (pos.x-10)+'px'}).show();
+      $(menu).css({top: (pos.y-10)+'px', left: (pos.x-10)+'px'}).show();
       return true;
     }
 
@@ -1227,9 +1227,9 @@ function rcube_webmail()
 
   this.drag_menu_action = function(action)
   {
-    var menu = $('#'+this.gui_objects.message_dragmenu);
+    var menu = this.gui_objects.message_dragmenu;
     if (menu) {
-      menu.hide();
+      $(menu).hide();
     }
     this.command(action, this.env.drag_target);
     this.env.drag_target = null;