kmribti/roundcube

commit ba7646e50203b7cc91ac57cc5dad96aded8dd745

- Fix handling of binary attachments encoded with quoted-printable (#1488065)
git-svn-id: https://svn.roundcube.net/trunk@5204 208e9e7b-5314-0410-a742-e7e81cd9613c

roundcubemail/program/include/rcube_imap.php
@@ -2386,6 +2386,9 @@ class rcube_imap
             return true;
         }
 
+        // Remove NULL characters (#1486189)                                                                                      
+        $body = str_replace("\x00", '', $body);
+
         // convert charset (if text or message part)
         if ($body && !$skip_charset_conv &&
             preg_match('/^(text|message)$/', $o_part->ctype_primary)

roundcubemail/program/include/rcube_imap_generic.php
@@ -2470,8 +2470,6 @@ class rcube_imap_generic
                 } else if ($mode == 2) {
                     $line = rtrim($line, "\t\r\0\x0B");
                     $line = quoted_printable_decode($line);
-                    // Remove NULL characters (#1486189)
-                    $line = str_replace("\x00", '', $line);
                 // UUENCODE
                 } else if ($mode == 3) {
                     $line = rtrim($line, "\t\r\n\0\x0B");

commit 112765f78771686162f42d1f0afae7e33d7d6bff

- Fix forwarded attachments, broken in r5204
git-svn-id: https://svn.roundcube.net/trunk@5253 208e9e7b-5314-0410-a742-e7e81cd9613c

roundcubemail/program/include/rcube_imap.php
@@ -2390,17 +2390,17 @@ class rcube_imap
             return true;
         }
 
-        // Remove NULL characters (#1486189)
-        $body = str_replace("\x00", '', $body);
-
         // convert charset (if text or message part)
-        if ($body && !$skip_charset_conv &&
-            preg_match('/^(text|message)$/', $o_part->ctype_primary)
-        ) {
-            if (!$o_part->charset || strtoupper($o_part->charset) == 'US-ASCII') {
-                $o_part->charset = $this->default_charset;
+        if ($body && preg_match('/^(text|message)$/', $o_part->ctype_primary)) {
+            // Remove NULL characters (#1486189)
+            $body = str_replace("\x00", '', $body);
+
+           if (!$skip_charset_conv) {
+                if (!$o_part->charset || strtoupper($o_part->charset) == 'US-ASCII') {
+                    $o_part->charset = $this->default_charset;
+                }
+                $body = rcube_charset_convert($body, $o_part->charset);
             }
-            $body = rcube_charset_convert($body, $o_part->charset);
         }
 
         return $body;